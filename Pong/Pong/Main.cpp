20752ae4da27935ca0cae7efe340f673075225b8
Mert Salık <mertsalik.itu@gmail.com>
2013-04-03 03:43:27 +0300

    Sample screen implemented.

--- Pong/Pong/Main.cpp
+++ Pong/Pong/Main.cpp
@@ -1,8 +1,44 @@
 #include "Common.h"
+#include "GLHelper.h"
+#include "Model.h"
+
+Model *bunny;
+
+Model *capsule;
+
+//Camera *camera;
+
+void Draw(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+
+	bunny->draw(GLHelper::getInstance().currentCamera);
+
+	glutSwapBuffers();
+	GLHelper::getInstance().increaseFrameCount();
+}
 
 int main(int arg, char*argv[]){
-	
-	
+	try{
+		GLHelper::getInstance().InitGL();
 
-	return 0;
+		bunny = new Model("bunny.obj",
+			"vert_shader.vert",
+			"frag_shader.frag");
+
+		GLHelper::getInstance().DRAW = Draw;
+		GLHelper::getInstance().BindGUIFunctions();
+		GLHelper::getInstance().timerCallback(0);
+		glutMainLoop();
+		return 0;
+	}catch(CustomException& cex){
+		cout << "ERROR!!" << endl;
+		cout<< cex.what() << endl;
+		exit(0);
+	}catch(exception& ex){
+		cout << "ERROR!!" << endl;
+		cout<< ex.what() << endl;
+		exit(0);
+	}
 }
